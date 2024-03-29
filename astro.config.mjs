4bff22946461c53b7838351c9ba3ba7239e57641
jakobnutson <51325437+jakobnutson@users.noreply.github.com>
2024-03-29 17:20:50 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,12 @@ export default defineConfig({
     base: '/fa',
     integrations: [
         mdx(),
+         partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
         sitemap(),
         tailwind({
             applyBaseStyles: false
